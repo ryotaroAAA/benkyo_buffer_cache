14c8f8ac521c108d2201bf2866f883105ad56dcb
ryotaro <ryo.ex3991@gmail.com>
2021-04-17 14:59:30 +0900

    buf

--- main.cpp
+++ main.cpp
@@ -8,27 +8,72 @@
 #include <vector>
 #include <string.h>
 
+/* data structure */
+typedef enum {
+    busy,
+    dwrite,
+    old
+} buf_stat_t;
+
 typedef struct buffer {
+    uint8_t id;
     uint8_t dev_no;
     uint8_t blk_no;
-    uint8_t stat;
+    buf_stat_t stat;
     struct buffer *free_next;
     struct buffer *free_prev;
     struct buffer *hash_next;
     struct buffer *hash_prev;
 } BUF;
 
-#define B_BUSY (1<<0)
-#define B_DWRITE (1<<1)
-#define B_OLD (1<<2)
-
-void createPool(BUF **buf, int hoge);
-void display(BUF *buf);
-void display1(BUF *buf);
+/* function */
+void create_pool(BUF **first, size_t size);
+void show_buf(BUF *first);
 
 BUF* getblk(uint8_t dev_no, uint8_t blk_no);
 void brelese(BUF *buf);
 
+/* global variable */
+BUF *bfree_list = NULL;
+BUF hash_que[5] = {};
+
+/* code */
+void show_buf(BUF *first) {
+    BUF *temp = first;
+    while (temp->free_next != first) {
+        printf("id    :%5lu\n", temp->id);
+        printf("dev_no:%5lu\n", temp->dev_no);
+        printf("blk_no:%5lu\n", temp->blk_no);
+        temp = temp->free_next;
+    }
+}
+
+void create_buf_pool(BUF **first, size_t size) {
+    BUF *temp = NULL;
+    for (size_t i = 0; i < size; i++){
+        BUF *new_buf = (BUF *)new BUF[1];
+        new_buf->id = i;
+        if (*first == NULL) {
+            *first = new_buf;
+            temp = new_buf;
+            new_buf->free_next = new_buf;
+            new_buf->free_prev = new_buf;
+            new_buf->free_next = new_buf;
+            new_buf->free_prev = new_buf;
+        } else {
+            temp->free_next = new_buf;
+            temp->hash_next = new_buf;
+            new_buf->free_next = (*first);
+            new_buf->hash_next = (*first);
+            new_buf->free_prev = temp;
+            new_buf->hash_prev = temp;
+            temp = new_buf;
+        }
+    }
+}
+
 int main (int argc, char* argv[]) {
-    printf("hello world!");
+    create_buf_pool(&bfree_list, 2);
+    show_buf(bfree_list);
+    return 0;
 }
